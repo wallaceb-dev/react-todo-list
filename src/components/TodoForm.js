797291c9169948c360fd5446ad60cd270447d2b5
Wallace Belem Teixeira <wallaceb.teixeira@gmail.com>
2022-01-13 21:58:26 -0300

    Todo functionality done

---
+++ src/components/TodoForm.js
@@ -0,0 +1,46 @@
+import React, { useState, useEffect, useRef } from 'react';
+
+function TodoForm(props) {
+  const [input, setInput] = useState('');
+
+  const inputRef = useRef(null);
+
+  useEffect(() => {
+    inputRef.current.focus();
+  });
+
+  const handleChange = (e) => {
+    setInput(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    props.onSubmit({
+      id: Math.floor(Math.random() * 100000),
+      text: input,
+    });
+
+    setInput('');
+  };
+
+  return (
+    <div>
+      <form className="todo-form" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Add a todo"
+          value={input}
+          name="text"
+          className="todo-input"
+          onChange={handleChange}
+          autoComplete="off"
+          ref={inputRef}
+        />
+        <button className="todo-button">Add todo</button>
+      </form>
+    </div>
+  );
+}
+
+export default TodoForm;
